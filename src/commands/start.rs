7527f3c4329cd0c1ff81e4b72a6cdb3a6449854a
hashkid <hashkid@side.one>
2024-08-28 20:38:56 +0800

    optimize start option

--- src/commands/start.rs
+++ src/commands/start.rs
@@ -5,19 +5,13 @@ use crate::app::{config::Config, relayer, signer};
 pub async fn execute(home: &str, relayer: bool, signer: bool) {
     
     let conf = Config::from_file(home).unwrap();
-    let conf_2 = conf.clone();
-    // let s = thread::spawn(move || {
-    //     signer::run_signer_daemon(conf);
-    // });
-    // let r = thread::spawn(move || {
-    //     relayer::run_relayer_daemon(conf_2);
-    // });
 
-    if relayer && signer {
-        join!(signer::run_signer_daemon(conf), relayer::run_relayer_daemon(conf_2));
-    } else if relayer {
-        relayer::run_relayer_daemon(conf_2).await;
-    } else if signer {
+    if relayer && !signer {
+        relayer::run_relayer_daemon(conf).await;
+    } else if signer && !relayer {
         signer::run_signer_daemon(conf).await;
+    } else {
+        // Start both signer and relayer as default
+        join!(signer::run_signer_daemon(conf.clone()), relayer::run_relayer_daemon(conf));
     }
 }
