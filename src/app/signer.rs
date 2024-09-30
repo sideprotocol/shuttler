573a39fd85f9c9e7ad6e8ebe1ba4159dd68c9656
keithsue <sufay28301@gmail.com>
2024-10-01 02:17:23 +0800

    increase max transmit size

--- src/app/signer.rs
+++ src/app/signer.rs
@@ -208,9 +208,10 @@ pub async fn run_signer_daemon(conf: Config) {
                 .heartbeat_interval(Duration::from_secs(10)) // This is set to aid debugging by not cluttering the log space
                 .validation_mode(gossipsub::ValidationMode::Strict) // This sets the kind of message validation. The default is Strict (enforce message signing)
                 .message_id_fn(message_id_fn) // content-address messages. No two messages of the same content will be propagated.
+                .max_transmit_size(1000000)
                 .build()
                 .map_err(|msg| io::Error::new(io::ErrorKind::Other, msg))?; // Temporary hack because `build` does not return a proper `std::error::Error`.
-
+            
             // build a gossipsub network behaviour
             let gossip = gossipsub::Behaviour::new(
                 gossipsub::MessageAuthenticity::Signed(key.clone()),
