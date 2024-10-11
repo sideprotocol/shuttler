0ac3a5ee204fa0ac8cdc90dd7bce13ad438faaa9
hashkid <ping@side.one>
2024-10-11 18:38:41 +0800

    clean code

--- src/helper/encoding.rs
+++ src/helper/encoding.rs
@@ -1,9 +1,6 @@
-use std::collections::BTreeMap;
-
 use base64::{engine::general_purpose::STANDARD, Engine};
 use bitcoin_hashes::sha256;
 use bitcoin_hashes::Hash;
-use frost_secp256k1_tr::Identifier;
 
 pub fn to_base64(input: &[u8]) -> String {
     // base64::encode(data)
