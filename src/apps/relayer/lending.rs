8a41dfa6e10ae7c38b4213e76a300dd46ac74218
keithsue <sufay28301@gmail.com>
2025-03-26 01:38:43 +0800

    handle signed cets

--- src/apps/relayer/lending.rs
+++ src/apps/relayer/lending.rs
@@ -15,10 +15,7 @@ use crate::{
 };
 
 use cosmos_sdk_proto::{cosmos::tx::v1beta1::BroadcastTxResponse, Any};
-use side_proto::side::{
-    liquidation::LiquidationStatus,
-    lending::{MsgApprove},
-};
+use side_proto::side::{dlc, lending::MsgApprove, liquidation::LiquidationStatus};
 
 const EVENT_TYPE_APPLY: &str = "apply";
 const EVENT_ATTRIBUTE_KEY_VAULT: &str = "vault";
@@ -179,6 +176,7 @@ fn parse_and_save_vaults(relayer: &Relayer, txs_results: Option<Vec<abci::types:
 
 async fn parse_and_handle_cets(relayer: &Relayer, end_block_events: Option<Vec<abci::Event>>) {
     let mut loan_ids = vec![];
+    let mut cet_types = vec![];
 
     end_block_events.unwrap_or(vec![]).iter().for_each(|event| {
         if event.kind == EVENT_TYPE_GENERATE_SIGNED_CET {
@@ -186,15 +184,22 @@ async fn parse_and_handle_cets(relayer: &Relayer, end_block_events: Option<Vec<a
                 if attr.key_str().unwrap() == EVENT_ATTRIBUTE_KEY_LOAN_ID {
                     let loan_id = attr.value_str().unwrap().to_string();
 
-                    debug!("Signed liquidation cet found on side, loan id: {}", loan_id);
+                    debug!("Signed cet found on side, loan id: {}", loan_id);
                     loan_ids.push(loan_id);
+                }
+
+                if attr.key_str().unwrap() == EVENT_ATTRIBUTE_KEY_CET_TYPE {
+                    let cet_type = attr.value_str().unwrap().to_string();
+
+                    debug!("Signed cet type: {}", cet_type);
+                    cet_types.push(cet_type);
                 }
             })
         };
     });
 
-    for loan_id in loan_ids {
-        handle_liquidation_cet(relayer, loan_id).await;
+    for (i, loan_id) in loan_ids.iter().enumerate() {
+        handle_cet(relayer, loan_id.to_string(), cet_types[i].clone()).await;
     }
 }
 
@@ -353,7 +358,7 @@ pub async fn send_deposit_tx(
     send_cosmos_transaction(&relayer.config(), any_msg).await
 }
 
-pub async fn handle_liquidation_cet(relayer: &Relayer, loan_id: String) {
+pub async fn handle_cet(relayer: &Relayer, loan_id: String, cet_type: String) {
     let dlc_meta = match get_loan_dlc_meta(&relayer.config.side_chain.grpc, loan_id.clone()).await {
         Ok(resp) => match resp.into_inner().dlc_meta {
             Some(dlc_meta) => dlc_meta,
@@ -368,24 +373,35 @@ pub async fn handle_liquidation_cet(relayer: &Relayer, loan_id: String) {
         }
     };
 
-    if dlc_meta.signed_liquidation_cet_hex.is_empty() {
-        error!("Liquidation cet not signed yet, loan id: {}", loan_id);
+    let signed_tx_hex = match cet_type.as_str() {
+        "0" => dlc_meta.liquidation_cet.unwrap_or_default().signed_tx_hex,
+        "1" => {
+            dlc_meta
+                .default_liquidation_cet
+                .unwrap_or_default()
+                .signed_tx_hex
+        }
+        "2" => dlc_meta.repayment_cet.unwrap_or_default().signed_tx_hex,
+        _ => "".to_string(),
+    };
+
+    if signed_tx_hex.is_empty() {
+        error!(
+            "Cet not signed yet, loan id: {}, cet type: {}",
+            loan_id, cet_type
+        );
         return;
     }
 
-    let signed_tx = Transaction::consensus_decode(
-        &mut hex::decode(dlc_meta.signed_liquidation_cet_hex)
-            .unwrap()
-            .as_slice(),
-    )
-    .unwrap();
+    let signed_tx =
+        Transaction::consensus_decode(&mut hex::decode(signed_tx_hex).unwrap().as_slice()).unwrap();
 
     match relayer.bitcoin_client.send_raw_transaction(&signed_tx) {
         Ok(txid) => {
-            debug!("Liquidation cet sent to bitcoin: {}", txid);
+            debug!("Cet sent to bitcoin: {}, cet type: {}", txid, cet_type);
         }
         Err(e) => {
-            error!("Failed to send liquidation cet to bitcoin: {}", e);
+            error!("Failed to send cet to bitcoin: {}", e);
         }
     }
 }
@@ -395,7 +411,10 @@ pub async fn handle_liquidation_settlement_tx(relayer: &Relayer, liquidation_id:
         Ok(resp) => match resp.into_inner().liquidation {
             Some(liquidation) => liquidation,
             None => {
-                error!("No liquidation exists on side, liquidation id: {}", liquidation_id);
+                error!(
+                    "No liquidation exists on side, liquidation id: {}",
+                    liquidation_id
+                );
                 return;
             }
         },
@@ -409,7 +428,10 @@ pub async fn handle_liquidation_settlement_tx(relayer: &Relayer, liquidation_id:
     };
 
     if liquidation.status != LiquidationStatus::Settled as i32 {
-        error!("Liquidation not settled yet, liquidation id: {}", liquidation_id);
+        error!(
+            "Liquidation not settled yet, liquidation id: {}",
+            liquidation_id
+        );
         return;
     }
 
