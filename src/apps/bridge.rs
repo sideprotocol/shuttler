69e3c7c4c8984d6eacdaecd24c50badee8f18874
keithsue <sufay28301@gmail.com>
2025-05-15 12:17:36 +0800

    implement noop for bridge refresh handler for now

--- src/apps/bridge.rs
+++ src/apps/bridge.rs
@@ -262,7 +262,7 @@ impl SignAdaptor for SignatureHandler {
 pub struct RefreshHandler;
 impl RefreshAdaptor for RefreshHandler {
     fn new_task(&self, ctx: &mut Context, events: &SideEvent) -> Option<Vec<Task>> {
-        todo!()
+        None
     }
 
     fn on_complete(&self, ctx: &mut Context, task: &mut Task, keys: Vec<(frost_adaptor_signature::keys::KeyPackage, frost_adaptor_signature::keys::PublicKeyPackage)>) {
