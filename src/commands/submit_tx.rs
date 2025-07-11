ee2a8604cce419853e7503bd64e152ba5e0726ac
keithsue <sufay28301@gmail.com>
2025-07-11 20:57:12 +0800

    add lending deposit tx submission

--- src/commands/submit_tx.rs
+++ src/commands/submit_tx.rs
@@ -4,7 +4,10 @@ use bitcoin::Txid;
 use tracing::Level;
 use tracing_subscriber;
 
-use crate::{apps::relayer::{bridge::check_and_handle_tx_by_hash, Relayer}, config::Config};
+use crate::{
+    apps::relayer::{bridge, lending, Relayer},
+    config::Config,
+};
 
 pub async fn execute(home: &str, hash: &String) {
     tracing_subscriber::fmt().with_max_level(Level::INFO).init();
@@ -14,5 +17,6 @@ pub async fn execute(home: &str, hash: &String) {
     let conf = Config::from_file(home).unwrap();
     let relayer = Relayer::new(conf);
 
-    check_and_handle_tx_by_hash(&relayer, &txid).await
+    bridge::check_and_handle_bitcoin_tx_by_hash(&relayer, &txid).await;
+    lending::check_and_handle_bitcoin_tx_by_hash(&relayer, &txid).await;
 }
